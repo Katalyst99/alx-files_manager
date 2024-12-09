1f3dc9e14a1d30b42e64e0f178ea80dff9ac1e96
Katalyst99 <katleho333@gmail.com>
2024-12-09 05:29:44 -0800

    Additional fixes

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,36 +3,32 @@ import dbClient from '../utils/db';
 
 class UsersController {
   static async postNew(req, res) {
-    const { email, password } = req.body;
-    if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-      res.end();
-      return;
+    try {
+      const { email, password } = req.body;
+      if (!email) {
+        return res.status(400).json({ error: 'Missing email' });
+      }
+      if (!password) {
+        return res.status(400).json({ error: 'Missing password' });
+      }
+      const users = dbClient.db().collection('users');
+      const user = await users.findOne({ email });
+      if (user) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+      const hashdPwd = sha1(password);
+      const newUser = {
+        email,
+        password: hashdPwd,
+      };
+      const output = await users.insertOne(newUser);
+      return res.status(201).json({
+        id: output.insertedId,
+        email,
+      });
+    } catch (err) {
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
-    if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-      res.end();
-      return;
-    }
-    const users = dbClient.db().collection('users');
-    const user = await users.findOne({ email });
-    if (user) {
-      res.status(400).json({ error: 'Already exist' });
-      res.end();
-      return;
-    }
-    const hashdPwd = sha1(password);
-    const newUser = {
-      email,
-      password: hashdPwd,
-    };
-    const output = await users.insertOne(newUser);
-    return res.status(201).json({
-      id: output.insertedId,
-      email,
-    });
-  } catch (err) {
-    return res.status(500).json({ error: 'Internal Server Error' });
   }
 }
 
